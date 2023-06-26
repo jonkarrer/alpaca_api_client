1e4d78f79ea3f420534bfa6c2f6269633d33f788
jkarrer <jkarrer@protect247.com>
2023-06-26 13:58:28 -0400

    feat: OTO ordering

--- src/order.rs
+++ src/order.rs
@@ -24,7 +24,6 @@ pub enum OrderStatus {
     Suspended,
 }
 
-/// A single order object
 #[derive(Deserialize, Debug)]
 pub struct Order {
     pub id: String,
@@ -61,14 +60,12 @@ pub struct Order {
     pub hwm: Option<String>,
 }
 
-/// Buy or sell
 #[derive(Debug, Clone, Copy)]
 pub enum OrderSide {
     Buy,
     Sell,
 }
 
-/// Place a market order
 pub fn place_market_order(stock_symbol: &str, qty: f32, trade_side: OrderSide) -> Order {
     let url = "https://paper-api.alpaca.markets/v2/orders";
 
@@ -92,7 +89,6 @@ pub fn place_market_order(stock_symbol: &str, qty: f32, trade_side: OrderSide) -
     res.expect("No Order In Response")
 }
 
-/// Place a trailing stop order
 pub fn place_trailing_stop_order(stock_symbol: &str, qty: f32, trail_percent: f32) -> Order {
     let url = "https://paper-api.alpaca.markets/v2/orders";
 
@@ -114,7 +110,6 @@ pub fn place_trailing_stop_order(stock_symbol: &str, qty: f32, trail_percent: f3
     res.expect("No Order In Response")
 }
 
-/// Place a bracket order
 pub fn place_bracket_order(
     stock_symbol: &str,
     qty: f32,
@@ -152,7 +147,64 @@ pub fn place_bracket_order(
     res.expect("No Order In Response")
 }
 
-/// Get all orders for account
+pub fn place_oto_take_profit_order(stock_symbol: &str, qty: f32, take_price: f32) -> Order {
+    let url = "https://paper-api.alpaca.markets/v2/orders";
+
+    let take_profit = (take_price * 100.0).round() / 100.0;
+
+    let oto_order = json!({
+        "symbol": stock_symbol,
+        "qty": qty.to_string(),
+        "side": "buy",
+        "type": "market",
+        "time_in_force": "gtc",
+        "order_class": "oto",
+        "take_profit": {
+            "limit_price": take_profit.to_string()
+        },
+    });
+
+    let err_message = format!("Failed To Place Take Profit Order {}", stock_symbol);
+    let r = request("POST", url)
+        .set("Content-Type", "application/json")
+        .send_json(oto_order)
+        .expect(&err_message);
+    let res: Option<Order> = r
+        .into_json()
+        .expect("Failed To Convert Take Profit Order To JSON");
+
+    res.expect("No Order Object In Response")
+}
+
+pub fn place_oto_stop_loss_order(stock_symbol: &str, qty: f32, stop_price: f32) -> Order {
+    let url = "https://paper-api.alpaca.markets/v2/orders";
+
+    let stop_loss = (stop_price * 100.0).round() / 100.0;
+    let stop_limit = ((stop_price - 1.0) * 100.0).round() / 100.0;
+    let bracket_order = json!({
+        "symbol": stock_symbol,
+        "qty": qty.to_string(),
+        "side": "buy",
+        "type": "market",
+        "time_in_force": "gtc",
+        "order_class": "oto",
+        "stop_loss": {
+            "stop_price": stop_loss.to_string(),
+            "limit_price": stop_limit.to_string()
+        }
+    });
+
+    let err_message = format!("Failed To Place Stop Loss Order {}", stock_symbol);
+    let r = request("POST", url)
+        .set("Content-Type", "application/json")
+        .send_json(bracket_order)
+        .expect(&err_message);
+    let res: Option<Order> = r
+        .into_json()
+        .expect("Failed To Convert Stop Loss Order To JSON");
+
+    res.expect("No Order Object In Response")
+}
 pub fn get_orders(query: Option<&str>) -> Vec<Order> {
     let url = "https://paper-api.alpaca.markets/v2/orders";
     let address = match query {
@@ -167,7 +219,6 @@ pub fn get_orders(query: Option<&str>) -> Vec<Order> {
         .expect("Could Not Parse Response Into Json")
 }
 
-/// Get an order by it's id
 pub fn get_order_by_id(order_id: &str) -> Order {
     let address = format!("https://paper-api.alpaca.markets/v2/orders/{order_id}");
 
